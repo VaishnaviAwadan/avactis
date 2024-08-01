cad3e03b87f131d311b9a08acc673ebf069db2d5
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2024-08-02 02:43:31 +0530

    Home and registration pages added

---
+++ src/main/java/com/avactis/pages/HomePage.java
@@ -0,0 +1,19 @@
+package com.avactis.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    WebDriver driver;
+
+    By registerLink = By.linkText("Register");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickRegister() {
+        driver.findElement(registerLink).click();
+    }
+}
+
