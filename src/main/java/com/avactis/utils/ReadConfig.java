fd10b6edd9f7606138b46a8fc20db4f0de5dcadf
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2024-08-04 14:20:26 +0530

    Added ReadConfig class to handle properties file

--- src/main/java/com/avactis/utils/ReadConfig.java
+++ src/main/java/com/avactis/utils/ReadConfig.java
@@ -1,5 +1,29 @@
 package com.avactis.utils;
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 public class ReadConfig {
+	
+	    private Properties properties;
 
-}
+	    public ReadConfig(String filePath) {
+	        properties = new Properties();
+	        try {
+	            FileInputStream fis = new FileInputStream("config/config.properties");
+	            properties.load(fis);
+	            fis.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    public String getProperty(String key) {
+	        return properties.getProperty(key);
+	    }
+
+	    public static void main(String[] args) {
+	        ReadConfig config = new ReadConfig("config.properties");
+	        System.out.println(config.getProperty("url")); // Example usage
+	    }
+	}
+
