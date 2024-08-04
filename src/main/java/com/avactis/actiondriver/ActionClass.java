36cb19e8ea89c86a28f9858d04f2d88c1f2158aa
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2024-08-04 13:41:41 +0530

    file modified

--- src/main/java/com/avactis/actiondriver/ActionClass.java
+++ src/main/java/com/avactis/actiondriver/ActionClass.java
@@ -1,5 +1,56 @@
 package com.avactis.actiondriver;
 
-public class ActionClass {
 
-}
+	import java.io.FileInputStream;
+	import java.io.IOException;
+	import java.util.Properties;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.WebElement;
+	import org.openqa.selenium.interactions.Actions;
+
+	public class ActionClass {
+	    private WebDriver driver;
+	    private Properties properties;
+
+	    public ActionClass(WebDriver driver) {
+	        this.driver = driver;
+	        loadProperties();
+	    }
+
+	    private void loadProperties() {
+	        properties = new Properties();
+	        try {
+	            FileInputStream input = new FileInputStream("config.properties");
+	            properties.load(input);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    public void navigateToBaseUrl() {
+	        String baseUrl = properties.getProperty("url");
+	        driver.get(baseUrl);
+	    }
+
+	    public void performHoverAction(WebElement element) {
+	        Actions actions = new Actions(driver);
+	        actions.moveToElement(element).perform();
+	    }
+
+	    public void performClickAction(WebElement element) {
+	        Actions actions = new Actions(driver);
+	        actions.moveToElement(element).click().perform();
+	    }
+
+	    public void performDoubleClickAction(WebElement element) {
+	        Actions actions = new Actions(driver);
+	        actions.moveToElement(element).doubleClick().perform();
+	    }
+
+	    public void performRightClickAction(WebElement element) {
+	        Actions actions = new Actions(driver);
+	        actions.moveToElement(element).contextClick().perform();
+	    }
+
+	
+	}
--- src/main/java/com/avactis/base/BaseClass.java
+++ src/main/java/com/avactis/base/BaseClass.java
@@ -1,5 +1,71 @@
 package com.avactis.base;
 
-public class BaseClass {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
+public class BaseClass {
+    public WebDriver driver;
+    public Properties properties;
+    public WebDriverWait wait;
+
+    @BeforeClass
+    public void setUp() {
+        loadProperties();
+        initializeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get(properties.getProperty("baseUrl"));
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    private void loadProperties() {
+        properties = new Properties();
+        try {
+            FileInputStream input = new FileInputStream("config.properties");
+            properties.load(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initializeDriver() {
+        String browser = properties.getProperty("browser");
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "path/to/geckodriver");
+            driver = new FirefoxDriver();
+        } else {
+            throw new IllegalArgumentException("Browser not supported: " + browser);
+        }
+    }
+
+    public void takeScreenshot(String fileName) {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        try {
+            FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE), new File("screenshots/" + fileName + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
